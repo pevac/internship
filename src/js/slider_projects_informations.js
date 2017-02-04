72f7cb5c2dcbbe9539831ec7ea2c80d47ab61c04
Верстюк Надежда <nadia.ver@i.ua>
2017-02-05 00:58:53 +0200

    bugFixed

--- src/js/slider_projects_informations.js
+++ src/js/slider_projects_informations.js
@@ -1,23 +1,29 @@
 $(document).ready(function(){
 	$('.item .effect_first').click(function(){//.active
-		$('.projects_information .information_first').css('display', 'block');
+		$('.projects_information .information_first').show().siblings('.information').not().hide();
 	});
 	$('.item .effect_second').click(function(){//.active
-		$('.projects_information .information_second').css('display', 'block');
+		$('.projects_information .information_second').show().siblings('.information').not().hide();
 	});
 	$('.item .effect_third').click(function(){//.active
-		$('.projects_information .information_third').css('display', 'block');
+		$('.projects_information .information_third').show().siblings('.information').not().hide();
 	});
 	$('.item .effect_fourth').click(function(){//.active
-		$('.projects_information .information_fourth').css('display', 'block');
+		$('.projects_information .information_fourth').show().siblings('.information').not().hide();
 	});
 	$('.item .effect_fiveth').click(function(){//.active
-		$('.projects_information .information_fiveth').css('display', 'block');
+		$('.projects_information .information_fiveth').show().siblings('.information').not().hide();
 	});
 	$('.item .effect_sixth').click(function(){//.active
-		$('.projects_information .information_sixth').css('display', 'block');
+		$('.projects_information .information_sixth').show().siblings('.information').not().hide();
 	});
 	$('.close_information').click(function(){
-		$('.information').css('display', 'none');
+		$(this).parent().css('display', 'none');
+	});
+	$('.step_left').click(function(){
+		$('.information').hide();
+	});
+	$('.step_right').click(function(){
+		$('.information').hide();
 	});
 });

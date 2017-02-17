fab29d777a09d87f23a58791a5daae5d50f33cec
Верстюк Надежда <nadia.ver@i.ua>
2017-02-17 18:48:44 +0200

    editModalsWindowsValidation

--- src/js/popup.js
+++ src/js/popup.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 		var log = login.value;
 		var pass = password.value;
 		if (log == 'hello' && pass == "world"){
-			$('.modal-dialog.send_success').show();
+			$('.modal-dialog.request').show();
+			$('.modal-dialog.singin').css('opacity', '0');
 		} else if(log === '' && pass === ''){
 			alert('Не введено дані!');
 		} else if (log == 'hello' && pass !== 'world'){
@@ -13,10 +14,33 @@ $(document).ready(function(){
 		} else if(log !== 'hello' && pass !== 'world'){
 			$('.modal-dialog.send_error_notAlumnus').show();
 		}
-		//$('.request').hide();
 		//$('.modal-dialog.send_success').show();
 	});
 	$('.close').click(function(){
 		$('.can_close').css('display', 'none');
 	});
+
+
+
+	//-----limit checkbox-----//
+	$(".checkbox").change(function () {
+        var maxAllowed = 2;
+        var cnt = $(".checkbox:checked").length;
+        if (cnt > maxAllowed) {
+            $(this).prop("checked", "");
+            //alert('Вибачте, але можна обрати лише' + maxAllowed + ' технології');
+        }
+    });
+
+
+	$('.send').click(function(){
+		var cnt = $(".checkbox:checked").length;
+		if (cnt < 1){
+			alert('Для подачі заявки необхідно вибрати хоча б один напрям.');
+		} else if (cnt >=1 && cnt <= 2){
+			$('.modal-dialog.send_success').show();
+			$('.modal-dialog.request').css('opacity', '0');
+		}
+	});
+
 });
